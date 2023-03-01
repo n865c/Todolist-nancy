c6799f21cd689a5172c725ba9288d50fa70ca548
unknown <nancykumari620@gmail.com>
2023-03-01 12:07:11 +0530

    Authentication

--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,4 +1,4 @@
-import { collection, DocumentData, getDocs } from 'firebase/firestore';
+import { collection, DocumentData, getDocs ,onSnapshot,doc,query} from 'firebase/firestore';
 import React,{useEffect, useState} from 'react';
 import {db} from '../../firebase';
 export const HomePage = () => {
@@ -7,10 +7,21 @@ export const HomePage = () => {
         const todosCol = collection(db, 'todos');
         const todoSnapshot = await getDocs(todosCol);
         const todoList = todoSnapshot.docs.map((doc)=> doc.data());
-        setTodos(todoList);}
-      useEffect(()=>{
-        getTodos();
-      },[]);
+        setTodos(todoList);
+      }
+        const q = query(collection(db, "todos"));
+        useEffect(() => {
+          const unsub = onSnapshot(q, (querySnapshot) => {
+              const todos: DocumentData[] = [];
+              querySnapshot.forEach((doc) => {
+                  todos.push(doc.data());
+                  
+              });
+              setTodos(todos);
+              console.log('Current todo: ', todos.join(', '));
+          });
+          return unsub;
+      }, []);
       const todoItems=todos?.map((todo:DocumentData)=>(
         <span>{todo.title}</span>
       ));
--- src/components/login-page/login-page.tsx
+++ src/components/login-page/login-page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { getAuth, GoogleAuthProvider, signInWithPopup,User } from "firebase/auth";
+import Button from '@mui/material/Button';
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
 export const LoginPage= () => {
+  const [user,setuser]=useState<User | null>(null);
+  const signInWithGoogle=()=>{
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential?.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    setuser(user);
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    console.log(error);
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
   return (
     <div>
-      Login
+    <Button variant="text" onClick={signInWithGoogle}>Login</Button>
+    
+    {user && <h1>{user.displayName}</h1>}
     </div>
   )
 }
