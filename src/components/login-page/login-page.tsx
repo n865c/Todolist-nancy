183e610e3171edd48048a82b9f2dc9c9287f0aac
unknown <nancykumari620@gmail.com>
2023-03-01 13:42:27 +0530

    SignUpLogin

--- src/components/login-page/login-page.tsx
+++ src/components/login-page/login-page.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react'
-import { getAuth, GoogleAuthProvider, signInWithPopup,User } from "firebase/auth";
+import React, { ChangeEvent, useState } from 'react'
+import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup,User } from "firebase/auth";
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 const provider = new GoogleAuthProvider();
 const auth = getAuth();
 export const LoginPage= () => {
   const [user,setuser]=useState<User | null>(null);
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  const setEmailValue=(event:ChangeEvent<HTMLInputElement>)=>{
+    setEmail(event.target.value);
+  };
+  const setPasswordValue=(event:ChangeEvent<HTMLInputElement>)=>{
+    setPassword(event.target.value);
+  }
   const signInWithGoogle=()=>{
     signInWithPopup(auth, provider)
   .then((result) => {
@@ -27,12 +36,57 @@ export const LoginPage= () => {
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
   });
-  }
+  };
+  const signUp=()=>{
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    setuser(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  };
+  const loginInWithEmailAndPassword=()=>{
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    setuser(user);
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  };
   return (
     <div>
-    <Button variant="text" onClick={signInWithGoogle}>Login</Button>
-    
+      <TextField id="filled-basic" 
+      value={email}
+      onChange={setEmailValue}
+      label="Email" 
+      variant="filled" />
+      <TextField id="filled-basic" 
+      label="Password"  
+      value={password} 
+      type="password"
+      onChange={setPasswordValue}
+      variant="filled" />
+      <Button variant="text" onClick={signUp}>
+        Signup
+      </Button>
+    <Button variant="text" onClick={loginInWithEmailAndPassword}>Login</Button>
+    <Button variant="text" onClick={signInWithGoogle}>Login with goggle</Button>
     {user && <h1>{user.displayName}</h1>}
     </div>
   )
-}
+
+  }
+
